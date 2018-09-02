a70a23ad9079f47315e8c4e80696151ef983829c
Nazam <nazamzahoor@yahoo.com>
2018-09-02 00:04:02 -0700

    find error

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,14 +5,14 @@ var path = require('path');
 // Routing 
 
 module.exports = function(app){
-    app.get("*", function(req, res){
-        res.sendFile(path.join(__dirname, "./app/public/home.html"));
+    app.get("/home", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
     app.get("/survey", function(req, res){
-        res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-    app.get("/home", function(req,res){
+    app.get("*", function(req,res){
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
